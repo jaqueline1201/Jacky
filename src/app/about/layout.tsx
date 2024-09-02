008da3e06c37684e30ed9f4e7c6c7449dd555c11
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-09-01 19:32:14 -0700

    navbar created
    the navbar was created at the layout of the root because I need the same navigation bar in all the subtree pages.

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const layout = (props) => {
+  return (
+    <div>
+      <h1>about layout</h1>
+      {props.children}
+    </div>
+  );
+};
+
+export default layout;
