c3367aa4a7b1eaebc75212b7f7f1981a23f08eb4
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-09-03 20:23:25 -0700

    css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,21 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className="container">
-        <div className="section">
+        <div className={styles.section}>
           <div className={styles.comment}>13 guisador a escoger</div>
           <div className={styles.comment}>Los mejores tacos de colima</div>
           <div className={styles.comment}>Taco que no cierra no es taco</div>
-          <div className={styles.comment}>Amo esos tacos no puedo comer solo uno</div>
+          <div className={styles.comment}>
+            Amo esos tacos no puedo comer solo uno
+          </div>
           <div className={styles.comment}> Y lo mejor... las salsas uff!</div>
-          <Image src={tacoImg} alt="Taco image" />
+          <Image src={tacoImg} alt="Taco image" width={100} height={100} />
         </div>
-        <div className="section">
+        <div className={styles.section}>
           <div className={styles.commentw}>Verduras frescas del día</div>
-          <div className={styles.commentw}>Las tortillas del día. Riquísimas!</div>
+          <div className={styles.commentw}>
+            Las tortillas del día. Riquísimas!
+          </div>
           <div className={styles.commentw}>Con unas bebidas bien frías</div>
           <div className="guisados-text">
             <div className="guisados">
@@ -40,9 +44,9 @@ export default function Home() {
               <h3>here put a picture</h3>
             </div>
           </div>
-          <Image src={chileImg} alt="Chile" />
+          <Image src={chileImg} alt="Chile" width={100} height={100} />
         </div>
-        <div className="section">
+        <div className={styles.section}>
           <h1>5 Salsas</h1>
           <h3>Lo que se hace bien, se siente y se disfruta</h3>
           <div className="salsa-images">
@@ -57,8 +61,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <Image src={drinkImg} alt="bebida" />
-        <div className="section">
+        <Image src={drinkImg} alt="bebida" width={100} height={100} />
+        <div className={styles.section}>
           <h1>Bebidas</h1>
           <h3>Agua fresca bien helada y refresco para acompañar</h3>
         </div>
