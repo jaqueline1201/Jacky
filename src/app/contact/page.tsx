86a43286a805abe7e64863e6bb5a62fb946a25a7
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-09-05 15:12:44 -0700

    style for the contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,35 @@
 import React from "react";
+import styles from "./contact.module.css";
+import { Alegreya } from "next/font/google";
 
 const page = () => {
   return (
     <div>
-      <h1>Contact Us!</h1>
-      <div className="container">
-        <label htmlFor="">Name</label>
-        <input type="text" required />
-        <label htmlFor="">Email</label>
-        <input type="text" required />
-        <label htmlFor="">Phone number</label>
-        <input type="text" />
-        <label htmlFor="">Message</label>
-        <input type="text" required />
-        <button>Submit</button>
+      <div className={styles.sections}>
+        <h1 className={styles.title}>Contacto</h1>
+      </div>
+      <div className={styles.sections}>
+        <form className={styles.container}>
+          <div className={styles.block}>
+            <label htmlFor="frm-name">Nombre</label>
+            <input id="frm-name" type="text" name="name" required />
+          </div>
+          <div className={styles.block}>
+            <label htmlFor="frm-phone">Teléfono</label>
+            <input id="frm-phone" type="text" name="phone" required />
+          </div>
+          <div className={styles.block}>
+            <label htmlFor="frm-email">Correo electronico</label>
+            <input id="frm-email" type="text" name="email" required />
+          </div>
+          <div className={styles.block}>
+            <label htmlFor="frm-messsage">Mensaje</label>
+            <input id="frm-message" type="text" name="message" required />
+          </div>
+          <div className={styles.btn}>
+            <button>Enviar</button>
+          </div>
+        </form>
       </div>
     </div>
   );
