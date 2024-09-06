596b1892052fdc71bc961ce6225356f13856d693
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-09-06 13:30:15 -0700

    chiles img position between two sections

--- src/app/components/sections/salsa.tsx
+++ src/app/components/sections/salsa.tsx
@@ -7,35 +7,49 @@ import style from "../../page.module.css";
 
 export const Salsa = () => {
   return (
-    <div>
-      <Image src={chileImg} alt="Chile" width={100} height={100} />
-      <div className={styles.section}>
-        <div className={style.containerSection}></div>
-        <h1>5 Salsas</h1>
-        <h3>Lo que se hace bien, se siente y se disfruta</h3>
-        <div className={style.productContainer}>
-          <div className={style.descriptionandImgB}>
-            <div className={style.productImage}>
-              <Image src={salsaImg} alt="salsa" width={200} height={200} />
+    <div className={styles.container}>
+      <div className={styles.images}>
+        <Image
+          src={chileImg}
+          alt="Chile"
+          width={1400}
+          height={1000}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+        />
+      </div>
+      <div className={styles.ndpart}>
+        <div className={styles.section}>
+          <div className={style.containerSection}></div>
+          <h1>5 Salsas</h1>
+          <h3>Lo que se hace bien, se siente y se disfruta</h3>
+          <div className={style.productContainer}>
+            <div className={style.descriptionandImgB}>
+              <div className={style.productImage}>
+                <Image src={salsaImg} alt="salsa" width={200} height={200} />
+              </div>
+              <div className={style.productText}>
+                <h3>Photo description</h3>
+              </div>
             </div>
-            <div className={style.productText}>
-              <h3>Photo description</h3>
+            <div className={style.descriptionandImgB}>
+              <div className={style.productImage}>
+                <Image src={salsaImg} alt="salsa" width={200} height={200} />
+              </div>
+              <div className={style.productText}>
+                <h3>Photo description</h3>
+              </div>
             </div>
-          </div>
-          <div className={style.descriptionandImgB}>
-            <div className={style.productImage}>
-              <Image src={salsaImg} alt="salsa" width={200} height={200} />
-            </div>
-            <div className={style.productText}>
-              <h3>Photo description</h3>
-            </div>
-          </div>
-          <div className={style.descriptionandImgB}>
-            <div className={style.productImage}>
-              <Image src={salsaImg} alt="salsa" width={200} height={200} />
-            </div>
-            <div className={style.productText}>
-              <h3>Photo description</h3>
+            <div className={style.descriptionandImgB}>
+              <div className={style.productImage}>
+                <Image src={salsaImg} alt="salsa" width={200} height={200} />
+              </div>
+              <div className={style.productText}>
+                <h3>Photo description</h3>
+              </div>
             </div>
           </div>
         </div>
